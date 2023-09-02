7029597751baecfda0dfe5929873ffaafaeba8b1
Florian Macherey <flo.macherey@gmail.com>
2023-09-02 12:34:47 +0200

    prepare data

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # 6-2-1-rules
 6-2-1 rules generator
+
+Generate 6-2-1 rules. Data source: https://pad.medialepfade.net/6-2-1-Generator?both
+
+Inspired by CCC-events 6-2-1 Rule: 6 hours of sleep, 2 meals, 1 shower - repeat daily.
--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,27 @@
 import './style.css'
 
-const getRandomItem = (items: Array<string>) => items[Math.floor(Math.random()*items.length)]
+const getRandomItem = (items: Array<string>): string => items[Math.floor(Math.random()*items.length)]
 
 const prepareData = async () => {
-  const data = await fetch("https://pad.medialepfade.net/6-2-1-Generator/download").then(r => r.text());
-  const dataArray = data.split("\n")
+  const data: string = await fetch("https://pad.medialepfade.net/6-2-1-Generator/download").then(r => r.text());
+  const dataArray: Array<string> = data.split("\n")
   const startIndex = dataArray.findIndex(item => item === "## Rows");
   // slice data and cleanup empty values
-  const parsedData = dataArray.slice(startIndex+1, data.length).filter(item => item);
+  const parsedData: Array<string> = dataArray.slice(startIndex+1, data.length).filter(item => item);
 
   // @todo be sure items are unique? 
-  const rulesRaw = [getRandomItem(parsedData), getRandomItem(parsedData), getRandomItem(parsedData)];
+  const rulesRaw: Array<string> = [getRandomItem(parsedData), getRandomItem(parsedData), getRandomItem(parsedData)];
 
   const rules = [
     rulesRaw[0].replace(/{x}/g, "6"),
     rulesRaw[1].replace(/{x}/g, "2"),
     rulesRaw[2].replace(/{x}/g, "1"),
-  ]
+  ];
 
-
+  const app = document.querySelector("#app");
+  if (app) {
+    app.innerHTML = rules.map(rule => `<p>${rule}</p>`).join("");
+  }
 
   console.log({parsedData, rules});
 }
